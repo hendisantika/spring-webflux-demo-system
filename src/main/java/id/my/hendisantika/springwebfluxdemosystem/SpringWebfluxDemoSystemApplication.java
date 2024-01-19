a32a75c62a4e07e6a7828887c570f05427ba9c07
hendisantika <hendisantika@yahoo.co.id>
2024-01-19 07:56:51 +0700

    Create starter project 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/springwebfluxdemosystem/SpringWebfluxDemoSystemApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springwebfluxdemosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxDemoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxDemoSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springwebfluxdemosystem/SpringWebfluxDemoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springwebfluxdemosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxDemoSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

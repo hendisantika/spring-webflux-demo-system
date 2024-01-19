77e1634f7f92952425cc0af125e5769c8a365510
hendisantika <hendisantika@yahoo.co.id>
2024-01-19 08:03:47 +0700

    Add StudentRepository 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/springwebfluxdemosystem/repository/StudentRepository.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.springwebfluxdemosystem.repository;
+
+import id.my.hendisantika.springwebfluxdemosystem.model.Student;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-webflux-demo-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/19/24
+ * Time: 08:03
+ * To change this template use File | Settings | File Templates.
+ */
+public interface StudentRepository extends ReactiveMongoRepository<Student, Integer> {
+}

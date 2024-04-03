c891402a0d9f659bf60a430dbf46a3973c5d3fab
hendisantika <hendisantika@yahoo.co.id>
2024-04-03 07:17:22 +0700

    Add passwordEncoder in SecurityConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springwebfluxdemosystem/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/springwebfluxdemosystem/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
@@ -20,6 +22,11 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
  */
 @EnableWebFluxSecurity
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public MapReactiveUserDetailsService userDetailsService() {
         UserDetails user = User

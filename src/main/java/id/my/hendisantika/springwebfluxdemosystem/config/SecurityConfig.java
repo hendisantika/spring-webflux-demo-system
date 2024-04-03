921250c773b02b172753f645971361c2466e0223
hendisantika <hendisantika@yahoo.co.id>
2024-04-03 07:16:41 +0700

    Add securityWebFilterChain in SecurityConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springwebfluxdemosystem/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/springwebfluxdemosystem/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package id.my.hendisantika.springwebfluxdemosystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +33,16 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
         return new MapReactiveUserDetailsService(user, admin);
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/students/admin")
+                .hasAuthority("ROLE_ADMIN")
+                .anyExchange()
+                .authenticated()
+                .and().httpBasic()
+                .and().build();
     }
 }

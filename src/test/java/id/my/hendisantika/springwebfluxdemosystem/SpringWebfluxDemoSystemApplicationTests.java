bfc274ac066fe8c3db8553d441ecb55b35aa5afc
hendisantika <hendisantika@yahoo.co.id>
2024-04-03 07:09:21 +0700

    Add onErrorExample 🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/springwebfluxdemosystem/SpringWebfluxDemoSystemApplicationTests.java
+++ src/test/java/id/my/hendisantika/springwebfluxdemosystem/SpringWebfluxDemoSystemApplicationTests.java
@@ -24,4 +24,12 @@ class SpringWebfluxDemoSystemApplicationTests {
         Flux<Integer> fluxAmounts = Flux.just(10, 20, 30);
         Flux.zip(fluxFruits, fluxColors, fluxAmounts).subscribe(System.out::println);
     }
+
+    @Test
+    public void onErrorExample() {
+        Flux<String> fluxCalc = Flux.just(-1, 0, 1)
+                .map(i -> "10 / " + i + " = " + (10 / i));
+        fluxCalc.subscribe(value -> System.out.println("Next: " + value),
+                error -> System.err.println("Error: " + error));
+    }
 }

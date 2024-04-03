f8df6c1618bb99482e80d149b9529b2710a369ae
hendisantika <hendisantika@yahoo.co.id>
2024-04-03 07:10:46 +0700

    Add publishSubscribeExample 🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/springwebfluxdemosystem/SpringWebfluxDemoSystemApplicationTests.java
+++ src/test/java/id/my/hendisantika/springwebfluxdemosystem/SpringWebfluxDemoSystemApplicationTests.java
@@ -3,6 +3,8 @@ package id.my.hendisantika.springwebfluxdemosystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 @SpringBootTest
@@ -52,4 +54,41 @@ class SpringWebfluxDemoSystemApplicationTests {
                 .expectError(ArithmeticException.class)
                 .verify();
     }
+
+    @Test
+    public void publishSubscribeExample() {
+        Scheduler schedulerA = Schedulers.newParallel("Scheduler A");
+        Scheduler schedulerB = Schedulers.newParallel("Scheduler B");
+        Scheduler schedulerC = Schedulers.newParallel("Scheduler C");
+
+        Flux.just(1)
+                .map(i -> {
+                    System.out.println("First map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(schedulerA)
+                .map(i -> {
+                    System.out.println("Second map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(schedulerB)
+                .map(i -> {
+                    System.out.println("Third map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(schedulerC)
+                .map(i -> {
+                    System.out.println("Fourth map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(schedulerA)
+                .map(i -> {
+                    System.out.println("Fifth map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .blockLast();
+    }
+
+
+
 }

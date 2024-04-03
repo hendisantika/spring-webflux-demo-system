3feadb4fbccbdc830140a01795aa74798cdc019b
hendisantika <hendisantika@yahoo.co.id>
2024-04-03 07:15:09 +0700

    Add SecurityConfig 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springwebfluxdemosystem/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.springwebfluxdemosystem.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-webflux-demo-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/3/24
+ * Time: 07:14
+ * To change this template use File | Settings | File Templates.
+ */
+@EnableWebFluxSecurity
+public class SecurityConfig {
+}
